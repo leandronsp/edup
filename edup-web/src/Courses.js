16e4596bc34adcae6f6d9b125340c403a20e1227
Leandro Maríngolo <leandronsp@gmail.com>
2018-09-01 14:33:32 +0100

    showCourse and improvement on UI

--- edup-web/src/Courses.js
+++ edup-web/src/Courses.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { List, Datagrid, TextField, RefreshButton, CreateButton, BooleanField,
+import { List, Datagrid, TextField, RefreshButton, CreateButton, ShowButton,
+  Show, SimpleShowLayout,
   DeleteButton, SimpleForm, Create, TextInput, CardActions } from 'react-admin';
 
 import PublishButton from './PublishButton'
@@ -15,11 +16,23 @@ export const CourseList = (props) => (
     <List {...props} title="All Courses" actions={<CourseActions />} bulkActions={null} >
         <Datagrid>
             <TextField source="name" />
-            <BooleanField source="published" />
+            <ShowButton />
             <PublishButton />
             <DeleteButton />
         </Datagrid>
     </List>
+);
+
+const CourseName = ({ record }) => {
+    return <span>Course {record ? `"${record.name}"` : ''}</span>;
+};
+
+export const CourseShow = (props) => (
+  <Show {...props} title={<CourseName />}>
+    <SimpleShowLayout>
+        <PublishButton />
+    </SimpleShowLayout>
+  </Show>
 );
 
 export const CourseCreate = (props) => (
--- edup-web/src/PublishButton.js
+++ edup-web/src/PublishButton.js
@@ -5,53 +5,37 @@ import ActionDone from '@material-ui/icons/Done';
 import ActionClear from '@material-ui/icons/Clear';
 import dataProvider from './dataProvider';
 import { refreshView } from 'ra-core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  buttonRed: { color: 'red' }
+};
 
 class PublishButton extends Component {
 
     handleClick = () => {
       const { record, showNotification } = this.props;
+      var data = { published: !record.published }
 
-      if (record.published) {
-        const updatedRecord = { published: false };
-
-        dataProvider(UPDATE, 'courses', { id: record.id, data: updatedRecord })
-          .then(() => {
-            showNotification('Course unpublished');
-            this.props.refreshView()
-          })
-          .catch((e) => {
-            console.error(e);
-            showNotification('Error: course not unpublished', 'warning')
-          });
-      } else {
-        const updatedRecord = { published: true };
-
-        dataProvider(UPDATE, 'courses', { id: record.id, data: updatedRecord })
-          .then(() => {
-            showNotification('Course published');
-            this.props.refreshView()
-          })
-          .catch((e) => {
-            console.error(e);
-            showNotification('Error: course not published', 'warning')
-          });
-      }
+      dataProvider(UPDATE, 'courses', { id: record.id, data: data })
+        .then(() => {
+          showNotification('Course updated');
+          this.props.refreshView()
+        })
+        .catch((e) => {
+          console.error(e);
+          showNotification('Error: could not update course', 'warning')
+        });
     }
 
     render() {
-        const { record } = this.props;
-        if (record.published) {
-          var label = "Unpublish"
-          var icon = <ActionClear />
-        } else {
-          label = "Publish"
-          icon = <ActionDone />
-        }
+        const { record, classes } = this.props;
+        var label = record.published ? "Unpublish" : "Publish"
+        var icon  = record.published ? <ActionClear /> : <ActionDone />
+        var buttonColor = record.published ? classes.buttonRed : null
 
         return (
-          <Button label={label} onClick={this.handleClick}>
-            {icon}
-          </Button>
+          <Button className={buttonColor} label={label} onClick={this.handleClick}>{icon}</Button>
         )
     }
 }
@@ -59,4 +43,4 @@ class PublishButton extends Component {
 export default connect(null, {
     showNotification,
     refreshView
-})(PublishButton);
+})(withStyles(styles)(PublishButton));
